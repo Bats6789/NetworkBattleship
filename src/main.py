fe9ee2c7761d775a3fc5a942ac10f3cf0e417f89
Bats6789 <bats23456789@gmail.com>
2023-12-30 21:40:07 -0600

    Added main

---
+++ src/main.py
@@ -0,0 +1,13 @@
+################################################
+# File: main.py                                #
+# Auth: Blake Wingard - bats23456789@gmail.com #
+# Desc: The main entry point for the project.  #
+################################################
+
+def main():
+    print('Hello python')
+    return 0
+
+
+if __name__ == '__main__':
+    main()
